2b0ccb8dbd7d65cda31a7f9880151af14b84c54e
agusmichaelsianipar <agus010sianipar@gmail.com>
2023-04-15 00:48:17 +0700

    DDD Design Pattern

---
+++ news-app/BTNews/Application/Middleware/AcceptJsonHeaderMiddleware.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace BTNews\Application\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class AcceptJsonHeaderMiddleware
+{
+    
+    public function handle(Request $request, Closure $next)
+    {
+        $request->headers->set('Accept', 'application/json');
+        
+        return $next($request);
+    }
+}
